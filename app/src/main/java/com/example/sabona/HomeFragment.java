a93b9b041cdd4b052ce3e7b1cbe6ea4b02320d45
NFunduk <funduk.nadjaa@gmail.com>
2026-05-08 16:12:12 +0200

    home page

--- app/src/main/java/com/example/sabona/HomeFragment.java
+++ app/src/main/java/com/example/sabona/HomeFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeFragment extends Fragment {
 
@@ -16,5 +19,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    // HomeFragment.java
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnHomePlay).setOnClickListener(v -> {
+            BottomNavigationView bottomNav = requireActivity().findViewById(R.id.bottomNav);
+            bottomNav.setSelectedItemId(R.id.play);
+        });
     }
 }
